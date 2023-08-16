ca50ff6acfb8c6907546c8368371e3fceae66a63
Philipp Moritz <pcmoritz@gmail.com>
2023-08-15 23:43:01 -0700

    Add simple slack bot for Ray Assistant (#1)
    You need to define the following slack bot environment variables:
    
    - SLACK_APP_TOKEN starts with "xapp-"
    - SLACK_BOT_TOKEN starts with "xoxb-"
    
    Once this gets more complex, the logic should live in the Ray Serve
    Deployment and the slack bot should call the deployment via REST. Or we
    structure it as a Serve application with two deployments (the main
    logic, and the slack bot that calls the main logic).
    
    ---------
    
    Signed-off-by: Philipp Moritz <pcmoritz@gmail.com>

---
+++ app/serve.py
@@ -0,0 +1,49 @@
+# You can run the whole script locally with
+# serve run serve:deployment
+
+import os
+
+from fastapi import FastAPI
+import query
+import ray
+from ray import serve
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+app = FastAPI()
+
+@ray.remote
+class SlackApp:
+    def __init__(self):
+        slack_app = App(token=os.environ["SLACK_BOT_TOKEN"])
+        self.agent = query.QueryAgent()
+
+        @slack_app.event("app_mention")
+        def event_mention(body, say):
+            text = body["event"]["text"][15:] # strip slack user id of bot mention
+            result = self.agent.get_response(text)
+            reply = result["answer"] + "\n" + "\n".join(result["sources"])
+            say(reply)
+
+        self.slack_app = slack_app
+
+    def run(self):
+        SocketModeHandler(self.slack_app, os.environ["SLACK_APP_TOKEN"]).start()
+
+ray.init(runtime_env={"env_vars": {
+    "DB_CONNECTION_STRING": os.environ["DB_CONNECTION_STRING"],
+    "OPENAI_API_KEY": os.environ["OPENAI_API_KEY"],
+    "SLACK_APP_TOKEN": os.environ["SLACK_APP_TOKEN"],
+    "SLACK_BOT_TOKEN": os.environ["SLACK_BOT_TOKEN"],
+}})
+
+@serve.deployment()
+@serve.ingress(app)
+class RayAssistantDeployment:
+    def __init__(self):
+        self.app = SlackApp.remote()
+        # Run the slack app in the background
+        self.runner = self.app.run.remote()
+
+# Deploy the Ray Serve application.
+deployment = RayAssistantDeployment.bind()
