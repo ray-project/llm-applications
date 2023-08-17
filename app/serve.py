adc86b467475da5960039e82f7990175af251ab2
Philipp Moritz <pcmoritz@gmail.com>
2023-08-16 20:22:08 -0700

    Refactor slack app to use Deployment
    Signed-off-by: Philipp Moritz <pcmoritz@gmail.com>

--- app/serve.py
+++ app/serve.py
@@ -5,7 +5,9 @@ import os
 
 import query
 import ray
+import requests
 from fastapi import FastAPI
+from pydantic import BaseModel
 from ray import serve
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -17,12 +19,11 @@ app = FastAPI()
 class SlackApp:
     def __init__(self):
         slack_app = App(token=os.environ["SLACK_BOT_TOKEN"])
-        self.agent = query.QueryAgent()
 
         @slack_app.event("app_mention")
         def event_mention(body, say):
             text = body["event"]["text"][15:]  # strip slack user id of bot mention
-            result = self.agent.get_response(text)
+            result = requests.post("http://127.0.0.1:8000/query/", json={"query": text}).json()
             reply = result["answer"] + "\n" + "\n".join(result["sources"])
             say(reply)
 
@@ -40,17 +41,34 @@ ray.init(
             "SLACK_APP_TOKEN": os.environ["SLACK_APP_TOKEN"],
             "SLACK_BOT_TOKEN": os.environ["SLACK_BOT_TOKEN"],
         }
-    }
+    },
+    ignore_reinit_error=True,
 )
+
+
+class Query(BaseModel):
+    query: str
+
+
+class Answer(BaseModel):
+    question: str
+    answer: str
+    sources: list[str]
 
 
 @serve.deployment()
 @serve.ingress(app)
 class RayAssistantDeployment:
     def __init__(self):
+        self.agent = query.QueryAgent()
         self.app = SlackApp.remote()
         # Run the slack app in the background
         self.runner = self.app.run.remote()
+
+    @app.post("/query")
+    def query(self, query: Query) -> Answer:
+        result = self.agent.get_response(query.query)
+        return Answer.parse_obj(result)
 
 
 # Deploy the Ray Serve application.
