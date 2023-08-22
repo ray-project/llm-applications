f4cc7e32799c72b1d76eff4705cbcfe153f88559
Philipp Moritz <pcmoritz@gmail.com>
2023-08-22 13:57:21 -0700

    training the router

---
+++ app/util.py
@@ -0,0 +1,52 @@
+from typing import Tuple
+
+import pandas as pd
+from ray.data import Dataset
+from sklearn.model_selection import train_test_split
+
+
+def stratify_split(
+    ds: Dataset,
+    stratify: str,
+    test_size: float,
+    shuffle: bool = True,
+    seed: int = 1234,
+) -> Tuple[Dataset, Dataset]:
+    """Split a dataset into train and test splits with equal
+    amounts of data points from each class in the column we
+    want to stratify on.
+
+    Args:
+        ds (Dataset): Input dataset to split.
+        stratify (str): Name of column to split on.
+        test_size (float): Proportion of dataset to split for test set.
+        shuffle (bool, optional): whether to shuffle the dataset. Defaults to True.
+        seed (int, optional): seed for shuffling. Defaults to 1234.
+
+    Returns:
+        Tuple[Dataset, Dataset]: the stratified train and test datasets.
+    """
+
+    def _add_split(df: pd.DataFrame) -> pd.DataFrame:  # pragma: no cover, used in parent function
+        """Naively split a dataframe into train and test splits.
+        Add a column specifying whether it's the train or test split."""
+        train, test = train_test_split(df, test_size=test_size, shuffle=shuffle, random_state=seed)
+        train["_split"] = "train"
+        test["_split"] = "test"
+        return pd.concat([train, test])
+
+    def _filter_split(df: pd.DataFrame, split: str) -> pd.DataFrame:  # pragma: no cover, used in parent function
+        """Filter by data points that match the split column's value
+        and return the dataframe with the _split column dropped."""
+        return df[df["_split"] == split].drop("_split", axis=1)
+
+    # Train, test split with stratify
+    grouped = ds.groupby(stratify).map_groups(_add_split, batch_format="pandas")  # group by each unique value in the column we want to stratify on
+    train_ds = grouped.map_batches(_filter_split, fn_kwargs={"split": "train"}, batch_format="pandas")  # combine
+    test_ds = grouped.map_batches(_filter_split, fn_kwargs={"split": "test"}, batch_format="pandas")  # combine
+
+    # Shuffle each split (required)
+    train_ds = train_ds.random_shuffle(seed=seed)
+    test_ds = test_ds.random_shuffle(seed=seed)
+
+    return train_ds, test_ds
