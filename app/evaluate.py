325269e95d64598d3642cffc9ebe888dbf1c2dd0
GokuMohandas <gokumd@gmail.com>
2023-09-07 14:30:19 -0700

    cleaned up notebook

---
+++ app/evaluate.py
@@ -0,0 +1,96 @@
+import re
+
+def extract_from_response(response):
+    # Define regular expressions for extracting values
+    answer_pattern = r'"answer"\s*:\s*"([^"]*)"'
+    score_pattern = r'"score"\s*:\s*([0-9]+)'
+    reasoning_pattern = r'"reasoning"\s*:\s*"([^"]*)"'
+
+    # Extract values using regular expressions
+    answer_match = re.search(answer_pattern, response)
+    score_match = re.search(score_pattern, response)
+    reasoning_match = re.search(reasoning_pattern, response)
+
+    # Convert
+    if answer_match and score_match and reasoning_match:
+        answer = answer_match.group(1)
+        score = float(score_match.group(1))
+        reasoning = reasoning_match.group(1)
+        return answer, score, reasoning
+
+    return "", "", ""
+
+
+def evaluate_responses(
+    experiment_name, reference_loc, response_loc,
+    evaluator, temperature, max_context_length,
+    system_content, assistant_content="",
+    num_samples=None):
+    
+    # Set credentials
+    set_credentials(llm=evaluator)
+    
+    # Load answers
+    with open(Path(reference_loc), "r") as f:
+        references = [item for item in json.load(f)][:num_samples]
+    with open(Path(response_loc), "r") as f:
+        generated = [item for item in json.load(f)["results"]][:num_samples]
+    assert len(references) == len(generated)
+
+    # Quality score
+    results = []
+    context_length = max_context_length - len(system_content + assistant_content)
+    for ref, gen in tqdm(zip(references, generated), total=len(references)):
+        assert ref["question"] == gen["question"]
+        user_content = str(
+            {
+                "question": gen["question"],
+                "generated_answer": gen["answer"],
+                "reference_answer": ref["answer"],
+            }
+        )[:context_length]
+
+        # Generate response
+        response = generate_response(
+            llm=evaluator,
+            temperature=temperature,
+            system_content=system_content,
+            assistant_content=assistant_content,
+            user_content=user_content,)
+
+        # Extract from response
+        score, reasoning = response.split("\n", 1)
+        result = {
+            "question": gen["question"],
+            "generated_answer": gen["answer"],
+            "reference_answer": ref["answer"],
+            "score": float(score),
+            "reasoning": reasoning.lstrip("\n"),
+            "sources": gen["sources"],
+        }
+        results.append(result)
+        clear_output(wait=True)
+        display(JSON(json.dumps(result, indent=2)))
+
+    # Save to file
+    evaluator_name = evaluator.split("/")[-1].lower()
+    evaluation_fp = Path(ROOT_DIR, EXPERIMENTS_DIR, "evaluations", f"{experiment_name}_{evaluator_name}.json")
+    evaluation_fp.parent.mkdir(parents=True, exist_ok=True)
+    config = {
+        "experiment_name": experiment_name,
+        "reference_loc": reference_loc,
+        "response_loc": response_loc,
+        "evaluator": evaluator,
+        "temperature": temperature,
+        "max_context_length": max_context_length,
+        "system_content": system_content,
+        "assistant_content": assistant_content,
+    }
+    evaluation = {
+        "config": config,
+        "retrieval_score": get_retrieval_score(references, generated),
+        "quality_score": np.mean([item["score"] for item in results if (item["score"] and item["reference_answer"])]),
+        "results": results,
+    }
+    with open(evaluation_fp, "w") as fp:
+        json.dump(evaluation, fp, indent=4)
--- app/query.py
+++ app/query.py
@@ -10,16 +10,24 @@ from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from pgvector.psycopg import register_vector
 
 
+def prepare_response(response, stream):
+    if stream:
+        answer = []
+        for chunk in response:
+            if "content" in chunk["choices"][0]["delta"].keys():
+                content = chunk["choices"][0]["delta"]["content"]
+                answer.append(content)
+                sys.stdout.write(content)
+                sys.stdout.flush()
+        return "".join([item for item in answer])
+    else:
+        return response["choices"][-1]["message"]["content"]
+
+
 def generate_response(
-    llm,
-    temperature=0.0,
-    stream=False,
-    system_content="",
-    assistant_content="",
-    user_content="",
-    max_retries=3,
-    retry_interval=60,
-):
+    llm, temperature=0.0, stream=False,
+    system_content="", assistant_content="", user_content="", 
+    max_retries=3, retry_interval=60):
     """Generate response from an LLM."""
     retry_count = 0
     while retry_count < max_retries:
@@ -32,26 +40,42 @@ def generate_response(
                     {"role": "system", "content": system_content},
                     {"role": "assistant", "content": assistant_content},
                     {"role": "user", "content": user_content},
-                ],
-            )
-
-            if stream:
-                answer = []
-                for chunk in response:
-                    if "content" in chunk["choices"][0]["delta"].keys():
-                        content = chunk["choices"][0]["delta"]["content"]
-                        answer.append(content)
-                        sys.stdout.write(content)
-                        sys.stdout.flush()
-                return "".join([item for item in answer])
-            else:
-                return response["choices"][-1]["message"]["content"]
+                ])
+            return prepare_response(response=response, stream=stream)
 
         except Exception as e:
-            print(f"Exception: {e}")
+            print (f"Exception: {e}")
             time.sleep(retry_interval)  # default is per-minute rate limits
             retry_count += 1
     return ""
+
+
+def get_embedding_model(embedding_model_name, model_kwargs, encode_kwargs):
+    if embedding_model_name == "text-embedding-ada-002":
+        embedding_model = OpenAIEmbeddings(
+            model=embedding_model_name,
+            model_kwargs=model_kwargs,
+            encode_kwargs=encode_kwargs,
+            openai_api_base=os.environ["OPENAI_API_BASE"],
+            openai_api_key=os.environ["OPENAI_API_KEY"])
+    else:
+        embedding_model = HuggingFaceEmbeddings(
+            model_name=embedding_model_name,
+            model_kwargs=model_kwargs,
+            encode_kwargs=encode_kwargs)
+    return embedding_model
+
+
+def get_sources_and_context(query, embedding_model, num_chunks):
+    embedding = np.array(embedding_model.embed_query(query))
+    with psycopg.connect(os.environ["DB_CONNECTION_STRING"]) as conn:
+        register_vector(conn)
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM document ORDER BY embedding <-> %s LIMIT %s", (embedding, num_chunks))
+            rows = cur.fetchall()
+            context = [{"text": row[1]} for row in rows]
+            sources = [row[2] for row in rows]
+    return sources, context
 
 
 class QueryAgent:
@@ -116,3 +140,61 @@ class QueryAgent:
             "answer": answer,
         }
         return result
+
+
+# Generate responses
+def generate_responses(
+    experiment_name, data_path, sections,
+    chunk_size, chunk_overlap, num_chunks,
+    embedding_model_name, 
+    llm, temperature, max_context_length, 
+    system_content, assistant_content="",
+    num_samples=None):
+    
+    # Build index
+    set_index(
+        sections=sections,
+        embedding_model_name=embedding_model_name,
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap)
+    
+    # Query agent
+    agent = QueryAgent(
+        embedding_model_name=embedding_model_name,
+        llm=llm,
+        temperature=temperature,
+        system_content=system_content,
+        assistant_content=assistant_content)
+
+    # Generate responses
+    results = []
+    with open(Path(data_path), "r") as f:
+        questions = [json.loads(item)["question"] for item in list(f)][:num_samples]
+    for query in tqdm(questions):
+        result = agent(query=query, num_chunks=num_chunks)
+        results.append(result)
+        clear_output(wait=True)
+        display(JSON(json.dumps(result, indent=2)))
+
+    # Save to file
+    responses_fp = Path(ROOT_DIR, EXPERIMENTS_DIR, "responses", f"{experiment_name}.json")
+    responses_fp.parent.mkdir(parents=True, exist_ok=True)
+    config = {
+        "experiment_name": experiment_name,
+        "data_path": data_path,
+        "chunk_size": chunk_size,
+        "chunk_overlap": chunk_overlap,
+        "num_chunks": num_chunks,
+        "embedding_model_name": embedding_model_name,
+        "llm": llm,
+        "temperature": temperature,
+        "max_context_length": max_context_length,
+        "system_content": system_content,
+        "assistant_content": assistant_content,
+    }
+    responses = {
+        "config": config,
+        "results": results,
+    }
+    with open(responses_fp, "w") as fp:
+        json.dump(responses, fp, indent=4)
