0c2d546a9d61bb165a789188a54a87727ebca160
Philipp Moritz <pcmoritz@gmail.com>
2023-12-19 13:07:12 -0800

    Fix cluster environment build

--- setup-pgvector.sh
+++ setup-pgvector.sh
@@ -3,7 +3,7 @@
 sudo apt install -y wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-sudo apt update && sudo apt install -y postgresql postgresql-contrib
+sudo apt update -y && sudo apt install -y postgresql postgresql-contrib
 # Install pgvector
 sudo apt install -y postgresql-server-dev-16
 pushd /tmp && git clone --branch v0.4.4 https://github.com/pgvector/pgvector.git && pushd pgvector && make && sudo make install && popd && popd
